1d29045f2a583157b6e4cdecd335fc02386160a7
KIMINJOONG <ijboym216@gmail.com>
2019-01-06 17:02:07 +0900

    3주차에 배운내용으로 수정

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,75 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_listview_exam/src/itemModel.dart';
+import 'package:flutter_listview_exam/src/widgets/custom_item_widget.dart';
 class Home extends StatelessWidget {
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
+    List<ItemModel> list = [
+      _buildItemModel("111"),
+      _buildItemModel("222"),
+      _buildItemModel("333"),
+    ];
+    final widgets = list.map((itemModel) => _buildChild(itemModel)).toList().cast<Widget>();
     return Scaffold(
       appBar: AppBar(
         title: Text('ListView Example'),
       ),
-      body: ListView(
-        children: <Widget>[
-          _buildChild(context),
-        ],
+      body: ListView.builder(
+        itemCount: widgets.length,
+        itemBuilder: (context, idx) {
+          return widgets[idx];
+        },
       )
     );
   }
-  _buildChild(context) {
-    TextStyle titleTextStyle = Theme.of(context).textTheme.title;
-    TextStyle descriptionTextStyle = Theme.of(context).textTheme.body1;
-    return Container(
-      height: 300,
-      child: Padding(padding: EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Image.asset(""),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(5.0),
-                width: double.maxFinite,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        width: double.maxFinite,
-                        color: Colors.red,
-                        alignment: Alignment.center,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Text("title"),
-                            Text("5"),
-                          ],
-                        )
-                      ),
-                    ),
-                    Expanded(
-                      child: Center(
-                        child: Text('책 설명', textAlign: TextAlign.center, style: TextStyle(fontSize: 20.0),),
-                      ),
-                    ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Text("지은이", style: descriptionTextStyle,),
-                          Text("지은이", style: descriptionTextStyle,),
-                          Text("지은이", style: descriptionTextStyle,),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+  _buildChild(ItemModel itemModel) {
+    return CustomItemWidget(itemModel : itemModel,);
+  }
 
+  _buildItemModel(String title) {
+    return ItemModel(
+      image: "assets/images/img_207001883.jpg",
+      title: title,
+      star: 1,
+      desc: "dssdss",
+      publisher: "한빛",
+      pubdate: "2019.1.5",
+      author: "홍길동",
     );
   }
 }
---
+++ lib/src/itemModel.dart
@@ -0,0 +1,12 @@
+class ItemModel {
+  String image;
+  String title;
+  int star;
+  String desc;
+  String author;
+  String publisher;
+  String pubdate;
+
+  ItemModel({this.image, this.title, this.star, this.desc, this.author,
+  this.publisher, this.pubdate});
+}
